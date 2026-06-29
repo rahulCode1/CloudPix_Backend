3a5842ead114bbea8804a5b9a59a9ae93f5b16f4
Rahul <pbrkumawat@gmail.com>
2026-06-29 11:21:53 +0530

    Add revoke permission feature

--- controller/album_controller.js
+++ controller/album_controller.js
@@ -1,291 +1,329 @@
-const Album = require("../model/album_model")
-const HttpError = require("../model/http_error")
-const User = require("../model/user_model")
-const Image = require("../model/image_model")
-const { validationResult } = require("express-validator")
+const Album = require("../model/album_model");
+const HttpError = require("../model/http_error");
+const User = require("../model/user_model");
+const Image = require("../model/image_model");
+const { validationResult } = require("express-validator");
 
 const createAlbum = async (req, res, next) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty) {
-        return next(new HttpError('Invalid album data.',
-            422,
-            errors.array()))
-    }
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 422));
+  }
 
-    const { name, description } = req.body
-    const ownerId = req.userId
-    try {
-        const album = new Album({ name, description, ownerId })
-        await album.save()
+  const { name, description } = req.body;
+  const ownerId = req.userId;
+  try {
+    const album = new Album({ name, description, ownerId });
+    await album.save();
 
-        res.status(201).json({
-            message: "New album added.",
-            album: album.toObject({ getters: true })
-        })
-    } catch (error) {
-        next(error)
-    }
-
-}
+    res.status(201).json({
+      message: "New album added.",
+      album: album.toObject({ getters: true }),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getAllAlbums = async (req, res, next) => {
-
-
-    try {
-        const albums = await Album.find().populate("ownerId").sort({ createdAt: -1 })
-        res.status(201).json({
-            message: "Albums fetched successfully.",
-            albums: albums.map(album => album.toObject({ getters: true }))
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const albums = await Album.find()
+      .populate("ownerId")
+      .sort({ createdAt: -1 });
+    res.status(201).json({
+      message: "Albums fetched successfully.",
+      albums: albums.map((album) => album.toObject({ getters: true })),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const updateAlbumDescription = async (req, res, next) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid data', 404, errors.array()))
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 404));
+  }
+
+  const albumId = req.params.albumId;
+  const userId = req.userId;
+  const { description } = req.body;
+
+  try {
+    const album = await Album.findById(albumId);
+
+    if (!album) {
+      return next(new HttpError(`No album find with that id.`, 404));
     }
 
-    const albumId = req.params.albumId
-    const userId = req.userId
-    const { description } = req.body
+    const albumOwnerId = album.ownerId.toString();
+    if (userId === albumOwnerId) {
+      album.description = description;
 
-    try {
-        const album = await Album.findById(albumId)
-
-        if (!album) {
-            return next(new HttpError(`No album find with that id.`, 404))
-        }
-
-        const albumOwnerId = album.ownerId.toString()
-        if (userId === albumOwnerId) {
-
-            album.description = description
-
-            await album.save()
-
-        } else {
-            return next(new HttpError("You'r not owner of that album, Only owner can edit album", 401, errors.array()))
-        }
-        res.status(201).json({
-            message: "Album updated.",
-            album: album.toObject({ getters: true })
-        })
-    } catch (error) {
-        next(error)
+      await album.save();
+    } else {
+      return next(
+        new HttpError(
+          "You'r not owner of that album, Only owner can edit album",
+          401,
+        ),
+      );
     }
-
-}
+    res.status(201).json({
+      message: "Album updated.",
+      album: album.toObject({ getters: true }),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const addUserToShareAlbum = async (req, res, next) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid data', 404, errors.array()))
-    }
-    const albumId = req.params.albumId
-    const { emails } = req.body
-    const userId = req.userId
-    try {
-        const album = await Album.findById(albumId)
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 422));
+  }
+  const { albumId } = req.params;
+  const { shareUserId } = req.body;
+  const userId = req.userId;
+  try {
+    const album = await Album.findById(albumId);
 
-        if (!album) {
-            return next(new HttpError(`No album find with that id.`, 404, errors.array()))
-        }
-
-        if (userId !== album.ownerId.toString()) {
-            return next(new HttpError("You'r not owner of that album, Only owner can edit album", 403, errors.array()))
-        }
-
-        const existingUsers = await User.find({ email: { $in: emails } })
-        const existingEmails = existingUsers.map(user => user.email)
-        const missingEmails = emails.filter(
-            email => !existingEmails.includes(email)
-        );
-
-        if (missingEmails.length > 0) {
-            return next(
-                new HttpError(
-                    `These users do not exist: ${missingEmails.join(", ")}`,
-                    422,
-                    errors.array()
-                )
-            );
-        }
-
-        const existingUserUserIdsArr = existingUsers.map(user => user._id.toString())
-
-        existingUserUserIdsArr.forEach(userId => {
-            if (!album.sharedWith.includes(userId)) {
-                album.sharedWith.push(userId)
-            }
-        })
-
-        await album.save()
-
-        res.status(201).json({
-            message: "Permission allowed successfully.",
-            album: album.toObject({ getters: true })
-        })
-    } catch (error) {
-        next(error)
+    if (!album) {
+      return next(new HttpError(`No album find with that id.`, 404));
     }
 
-}
+    if (userId !== album.ownerId.toString()) {
+      return next(
+        new HttpError(
+          "You'r not owner of that album, Only owner can edit album",
+          403,
+        ),
+      );
+    }
+
+    const existingUser = await User.findById(shareUserId);
+
+    if (!existingUser) {
+      return next(new HttpError("User not found.", 404));
+    }
+
+    const isAlbumAlreadyShared = album.sharedWith.some(
+      (id) => id.toString() === shareUserId,
+    );
+
+    if (isAlbumAlreadyShared) {
+      return next(new HttpError("Album already shared this user.", 409));
+    }
+
+    album.sharedWith.push(shareUserId);
+    await album.save();
+
+    res.status(200).json({
+      message: "Permission allowed successfully.",
+      album: album.toObject({ getters: true }),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const revokeAlbumAccess = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 422));
+  }
+  const { albumId } = req.params;
+  const { idForDenyAccess } = req.body;
+  const userId = req.userId;
+
+  try {
+    const album = await Album.findById(albumId);
+
+    if (!album) {
+      return next(new HttpError(`No album find with that id.`, 404));
+    }
+
+    if (userId !== album.ownerId.toString()) {
+      return next(
+        new HttpError(
+          "You'r not owner of that album, Only owner can revoke permission",
+          403,
+        ),
+      );
+    }
+
+    const hasAccess = album.sharedWith.some(
+      (user) => user.toString() === idForDenyAccess,
+    );
+
+    if (!hasAccess) {
+      return next(new HttpError("User doesn't have access.", 404));
+    }
+
+    if (idForDenyAccess === album.ownerId.toString()) {
+      return next(new HttpError("Owner access cannot be revoked.", 400));
+    }
+
+    album.sharedWith = album.sharedWith.filter(
+      (user) => user.toString() !== idForDenyAccess,
+    );
+    await album.save();
+
+    res.status(200).json({
+      message: "Access revoked successfully.",
+      album: album.toObject({ getters: true }),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteAlbum = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return next(new HttpError("Invalid album id.", 404, errors.array()));
+  }
 
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid album id.', 404, errors.array()))
+  const albumId = req.params.albumId;
+  const userId = req.userId;
+
+  try {
+    const album = await Album.findById(albumId);
+
+    if (!album) {
+      return next(
+        new HttpError(`No album find with that id.`, 404, errors.array()),
+      );
     }
 
-    const albumId = req.params.albumId
-    const userId = req.userId
-
-    try {
-        const album = await Album.findById(albumId)
-
-
-        if (!album) {
-            return next(new HttpError(`No album find with that id.`, 404, errors.array()))
-        }
-
-        if (userId !== album.ownerId.toString()) {
-            return next(new HttpError("You'r not owner of that album, Only owner can delete album", 401, errors.array()))
-        }
-
-        await Album.findByIdAndDelete(albumId)
-
-        await Image.deleteMany({ albumId })
-        res.status(200).json({
-            success: true,
-            message: `${album.name} deleted successfully.`,
-            albumId
-        })
-    } catch (error) {
-        next(error)
+    if (userId !== album.ownerId.toString()) {
+      return next(
+        new HttpError(
+          "You'r not owner of that album, Only owner can delete album",
+          401,
+        ),
+      );
     }
-}
 
+    await Album.findByIdAndDelete(albumId);
+
+    await Image.deleteMany({ albumId });
+    res.status(200).json({
+      success: true,
+      message: `${album.name} deleted successfully.`,
+      albumId,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getAllImageInAnAlbum = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid album id or album id not present.", 404),
+    );
+  }
 
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid album id or album id not present.',
-            404,
-            errors.array()))
+  const { albumId } = req.params;
+  const userId = req.userId;
+
+  try {
+    const album = await Album.findById(albumId).populate(
+      "sharedWith",
+      "_id email",
+    );
+
+    if (!album) {
+      return next(new HttpError("No album exist with that id.", 404));
     }
 
-    const albumId = req.params.albumId
-    const { tags } = req.query
-    const userId = req.userId
+    if (userId !== album.ownerId.toString()) {
+      const allowedUser = album.sharedWith.some(
+        (user) => user._id.toString() === userId,
+      );
 
-
-    let filter = {}
-
-    if (tags) {
-        const tagsArray = Array.isArray(tags) ? tags : [tags]
-        filter.tags = { $all: tagsArray }
+      if (!allowedUser) {
+        return next(new HttpError("Yourn't allow to see that album.", 422));
+      }
     }
 
+    const images = await Image.find({ albumId }).sort({
+      uploadedAt: -1,
+    });
 
-    try {
-        const album = await Album.findById(albumId)
+    const users = await User.find().sort({ createdAt: -1 });
 
-        if (!album) {
-            return next(new HttpError("No album exist with that id.", 404))
-        }
-
-
-        if (userId !== album.ownerId.toString()) {
-
-            const allowedUser = album.sharedWith.includes(userId)
-
-            if (!allowedUser) {
-
-                return next(new HttpError("Yourn't allow to see that album.", 422))
-
-            }
-
-        }
-
-
-
-        const images = await Image.find({ albumId, ...filter }).sort({ uploadedAt: -1 })
-        const users = await User.find().sort({ createdAt: -1 })
-
-       
-
-
-        res.status(200).json({
-            message: "Album fetched successfully.",
-            success: true,
-            users: users.map(user => user.toObject({ getters: true })),
-            album: album.toObject({ getters: true }),
-            images: images.map(image => image.toObject({ getters: true }))
-        })
-    } catch (error) { next(error) }
-}
+    res.status(200).json({
+      message: "Album fetched successfully.",
+      success: true,
+      users: users.map((user) => user.toObject({ getters: true })),
+      album: album.toObject({ getters: true }),
+      images: images.map((image) => image.toObject({ getters: true })),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getAllFavoriteImageInAnAlbum = async (req, res, next) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return next(new HttpError('Invalid album id or album id not present.',
-            404,
-            errors.array()))
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid album id or album id not present.", 404),
+    );
+  }
+
+  const albumId = req.params.albumId;
+  const userId = req.userId;
+
+  try {
+    const album = await Album.findById(albumId);
+
+    if (!album) {
+      return next(
+        new HttpError("No album exist with that id.", 404, errors.array()),
+      );
     }
 
-    const albumId = req.params.albumId
-    const userId = req.userId
+    if (req.userId !== album.ownerId.toString()) {
+      const allowedUser = album.sharedWith.includes(userId);
 
-    try {
-        const album = await Album.findById(albumId)
+      if (!allowedUser) {
+        return next(new HttpError("Yourn't allow to see that album.", 422));
+      }
+    }
 
-        if (!album) {
-            return next(new HttpError("No album exist with that id.", 404, errors.array()))
-        }
+    const images = await Image.find({ albumId, isFavorite: true }).sort({
+      uploadedAt: -1,
+    });
 
-        if (req.userId !== album.ownerId.toString()) {
-
-            const allowedUser = album.sharedWith.includes(userId)
-
-            if (!allowedUser) {
-
-                return next(new HttpError("Yourn't allow to see that album.", 422, errors.array()))
-
-            }
-
-        }
-
-        const images = await Image.find({ albumId, isFavorite: true }).sort({ uploadedAt: -1 })
-
-        res.status(201).json({
-            success: true,
-            message: "Favorite album images fetched successfully.",
-            images: images.map(image => image.toObject({ getters: true }))
-        })
-    } catch (error) { next(error) }
-}
-
-
-
+    res.status(201).json({
+      success: true,
+      message: "Favorite album images fetched successfully.",
+      images: images.map((image) => image.toObject({ getters: true })),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    getAllAlbums,
-    createAlbum,
-    updateAlbumDescription
-    , getAllImageInAnAlbum,
-    addUserToShareAlbum,
-    deleteAlbum,
-    getAllFavoriteImageInAnAlbum,
-
-}
+  getAllAlbums,
+  createAlbum,
+  updateAlbumDescription,
+  getAllImageInAnAlbum,
+  addUserToShareAlbum,
+  deleteAlbum,
+  getAllFavoriteImageInAnAlbum,
+  revokeAlbumAccess,
+};
--- routes/album_routes.js
+++ routes/album_routes.js
@@ -1,82 +1,103 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    createAlbum,
-    addUserToShareAlbum,
-    deleteAlbum,
-    updateAlbumDescription,
-    getAllAlbums,
-    getAllImageInAnAlbum,
-    getAllFavoriteImageInAnAlbum,
-
-
-} = require("../controller/album_controller")
-const { body, param } = require("express-validator")
-const auth_check = require("../middleware/auth_check")
+  createAlbum,
+  addUserToShareAlbum,
+  deleteAlbum,
+  updateAlbumDescription,
+  getAllAlbums,
+  getAllImageInAnAlbum,
+  getAllFavoriteImageInAnAlbum,
+  revokeAlbumAccess,
+} = require("../controller/album_controller");
+const { body, param } = require("express-validator");
+const auth_check = require("../middleware/auth_check");
 
 const addAlbumValidation = [
-    body("name").trim().notEmpty().withMessage("Album name must be required."),
-
-]
+  body("name").trim().notEmpty().withMessage("Album name must be required."),
+];
 
 const albumIdValidation = [
-    param("albumId")
-        .trim()
-        .notEmpty()
-        .withMessage("Album id is required")
-        .bail()
-        .isMongoId()
-        .withMessage("Album id must be mongoose id.")
-]
+  param("albumId")
+    .trim()
+    .notEmpty()
+    .withMessage("Album id is required")
+    .bail()
+    .isMongoId()
+    .withMessage("Album id must be mongoose id."),
+];
 
 const updateDescriptionValidation = [
   param("albumId")
-        .trim()
-        .notEmpty()
-        .withMessage("Album id is required")
-        .bail()
-        .isMongoId()
-        .withMessage("Album id must be mongoose id.")
+    .trim()
+    .notEmpty()
+    .withMessage("Album id is required")
+    .bail()
+    .isMongoId()
+    .withMessage("Album id must be mongoose id."),
+];
 
-]
+const addPermission = [
+  param("albumId")
+    .trim()
+    .notEmpty()
+    .withMessage("Album id is required")
+    .bail()
+    .isMongoId()
+    .withMessage("Album id must be mongoose id."),
+  body("shareUserId")
+    .trim()
+    .notEmpty()
+    .withMessage("User id required for give album access."),
+];
 
-const emailTypeValidation = [
-    body("emails").trim().notEmpty().
-        withMessage("Emails required").isEmail().
-        withMessage("Please enter a valid email address.")
-]
+const denyAccessValidation = [
+  param("albumId")
+    .trim()
+    .notEmpty()
+    .withMessage("Album id is required")
+    .bail()
+    .isMongoId()
+    .withMessage("Album id must be mongoose id."),
+  body("idForDenyAccess")
+    .trim()
+    .notEmpty()
+    .withMessage("User id required for revoke access."),
+];
 
-router.get("/", getAllAlbums)
-router.get("/:albumId/images/favorites",
-    albumIdValidation,
-    auth_check,
-    getAllFavoriteImageInAnAlbum)
+router.get("/", getAllAlbums);
+router.get(
+  "/:albumId/images/favorites",
+  albumIdValidation,
+  auth_check,
+  getAllFavoriteImageInAnAlbum,
+);
 
-router.get('/:albumId/images',
-    albumIdValidation,
-    auth_check,
-    getAllImageInAnAlbum)
+router.get(
+  "/:albumId/images",
+  albumIdValidation,
+  auth_check,
+  getAllImageInAnAlbum,
+);
 
-router.post('/',
-    addAlbumValidation,
-   auth_check,
-    createAlbum)
+router.post("/", addAlbumValidation, auth_check, createAlbum);
 
-router.put('/:albumId',
-    updateDescriptionValidation,
-    auth_check,
-    updateAlbumDescription)
+router.put(
+  "/:albumId",
+  updateDescriptionValidation,
+  auth_check,
+  updateAlbumDescription,
+);
 
-router.put('/:albumId/share',
-    emailTypeValidation,
-    auth_check,
-    addUserToShareAlbum)
-    
-router.delete('/:albumId',
-    albumIdValidation,
-    auth_check,
-    deleteAlbum)
+router.put("/:albumId/share", addPermission, auth_check, addUserToShareAlbum);
 
+router.patch(
+  `/:albumId/revokeAccess`,
+  denyAccessValidation,
+  auth_check,
+  revokeAlbumAccess,
+);
 
+router.delete("/:albumId", albumIdValidation, auth_check, deleteAlbum);
 
-module.exports = router
+module.exports = router;
